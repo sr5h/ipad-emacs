ba12ab18cefa6e03aa3b0f7f3514c2befbf82f98
srolisp <srolisp@gmail.com>
2020-07-21 08:42:18 +0000

    from hex to rgba.

--- 00-settings/srolisp-theme-blink.js
+++ 00-settings/srolisp-theme-blink.js
@@ -1,3 +1,3 @@
-t.prefs_.set('cursor-color', 'rgba(255, 0, 0, 0.5)');
-t.prefs_.set('foreground-color', '#ffe97a');
-t.prefs_.set('background-color', '#002b00');
+t.prefs_.set('cursor-color', 'rgba(205, 0, 0, 0.85)'); 
+t.prefs_.set('foreground-color', 'rgba(255, 233, 122, 0.85)'); // #FFE97A
+t.prefs_.set('background-color', 'rgba(0, 43, 0, 0.85'); // #002B00 -> 0, 43, 0

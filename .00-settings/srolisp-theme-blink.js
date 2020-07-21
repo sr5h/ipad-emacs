12c8f85ed171aea166b99fdd63362928ab5f46c7
srolisp <srolisp@gmail.com>
2020-07-21 08:48:51 +0000

    change color attributes of js.

---
+++ .00-settings/srolisp-theme-blink.js
@@ -0,0 +1,3 @@
+t.prefs_.set('cursor-color', 'rgba(205, 0, 0, 217)'); 
+t.prefs_.set('foreground-color', 'rgba(255, 233, 122, 217)'); // #FFE97A
+t.prefs_.set('background-color', 'rgba(0, 43, 0, 217)'); // #002B00 -> 0, 43, 0
--- 00-settings/srolisp-theme-blink.js
+++
@@ -1,3 +0,0 @@
-t.prefs_.set('cursor-color', 'rgba(205, 0, 0, 0.85)'); 
-t.prefs_.set('foreground-color', 'rgba(255, 233, 122, 0.85)'); // #FFE97A
-t.prefs_.set('background-color', 'rgba(0, 43, 0, 0.85'); // #002B00 -> 0, 43, 0

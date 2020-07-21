9a8219bca6a0b015aed6af0bcfca6f89856c37d2
srolisp <srolisp@gmail.com>
2020-07-21 08:25:11 +0000

    Change color mode-line and cursor color.

--- 00-settings/srolisp-theme-blink.js
+++ 00-settings/srolisp-theme-blink.js
@@ -1,3 +1,3 @@
-t.prefs_.set('cursor-color', 'rgba(1, 0, 0, 1)');
+t.prefs_.set('cursor-color', 'rgba(255, 0, 0, 0.5)');
 t.prefs_.set('foreground-color', '#ffe97a');
 t.prefs_.set('background-color', '#002b00');
